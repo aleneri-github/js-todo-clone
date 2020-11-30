b95434b1e59f51509607c21c1985c52302597b76
Alessandro <aleneri242@gmail.com>
2020-11-30 17:24:30 +0100

    aggiornamento to do list

--- js/script.js
+++ js/script.js
@@ -6,20 +6,39 @@
 $(document).ready(
   function() {
 
-    // var source = document.getElementById("entry-template").innerHTML;
+    var listaSerieTv = [
+      "BREAKING BAD",
+      "BAND OF BROTHERS",
+      "STAR TREK",
+      "I SIMPSON",
+      "IL TRONO DI SPADE",
+      "WEST WING",
+      "THE WIRE",
+      "I SOPRANO"
+    ];
+
     var source = $("#entry-template").html();
-    // console.log(source);
     var template = Handlebars.compile(source);
 
-    var context = {
-      title: "Regola dei terzi",
-      body: "La regola dei terzi è un accorgimento che è stato utilizzato per secoli dai pittori ed è tuttora molto diffuso nella composizione di una fotografia. Dividendo l'immagine in terzi e ponendo il soggetto in uno dei punti di intersezione delle linee immaginarie ottenute, si ritiene che l'immagine risulti più dinamica (rispetto ad una composizione che pone il soggetto al suo centro), ma armonica al tempo stesso. La regola è talmente popolare che alcune macchine fotografiche sono dotate di mirini con una griglia di suddivisione in terzi per aiutare il fotografo."
-     };
-    var html = template(context);
+    for (var i = 0; i < listaSerieTv.length; i++) {
 
-    // console.log(html);
+      var context = {
+        text: listaSerieTv[i]
 
-    $('#app').append(html);
+      };
+      var html = template(context);
+      $('#serie-tv').append(html);
+
+
+    }
+
+
+
+
+
+
+
+
 
 
 
